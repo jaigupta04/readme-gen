b8628102bf03820ba36907ad690e951184c84678
Jai Gupta <jaiwork04@gmail.com>
2024-10-28 17:28:35 +0530

    github auth

--- client/src/App.vue
+++ client/src/App.vue
@@ -204,8 +204,6 @@ export default {
       const credential = GithubAuthProvider.credentialFromResult(result);
       const token = credential.accessToken;
 
-      console.log(token)
-
       const user = result.user;
 
       this.loggedIn = true;
