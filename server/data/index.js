7357da6f06bfb82cfc463676023fcca8b6de48db
Jai Gupta <jaiwork04@gmail.com>
2025-03-24 17:48:39 +0530

    model change

--- server/data/index.js
+++ server/data/index.js
@@ -7,7 +7,7 @@ require('dotenv').config();
 
 const apiKey = process.env.GEMINI_API_KEY;
 const genAI = new GoogleGenerativeAI(apiKey);
-const model = genAI.getGenerativeModel({ model: "gemini-pro" });
+const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash-thinking-exp-01-21" });
 
 function fileWrite(filePath) {
 
