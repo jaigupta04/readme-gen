ed392201f056b007b9e574802652d52a8d0a1e24
Jai Gupta <jaiwork04@gmail.com>
2025-04-12 21:36:08 +0530

    vercel analytics

--- client/src/main.js
+++ client/src/main.js
@@ -3,6 +3,7 @@ import { createPinia } from 'pinia';
 import App from './App.vue';
 import router from './router';
 import { marked } from 'marked';
+import { inject } from '@vercel/analytics';
 
 // Configure marked options
 marked.setOptions({
@@ -15,6 +16,8 @@ marked.setOptions({
 // Create and mount the app
 const app = createApp(App);
 const pinia = createPinia();
+
+inject();
 
 app.use(pinia);
 app.use(router);
